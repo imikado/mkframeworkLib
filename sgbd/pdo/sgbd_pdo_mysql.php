f44401516a9c6e4f6e1545444d3347aea64998b9
imikado <imikado@gmail.com>
2025-03-08 15:57:16 +0100

    add new option for mysql connexion with SSL certificate

--- sgbd/pdo/sgbd_pdo_mysql.php
+++ sgbd/pdo/sgbd_pdo_mysql.php
@@ -15,79 +15,107 @@ You should have received a copy of the GNU Lesser General Public License
 along with Mkframework.  If not, see <http://www.gnu.org/licenses/>.
 
 */
-class sgbd_pdo_mysql extends abstract_sgbd_pdo{
-	
-	public static function getInstance($sConfig){
-		return self::_getInstance(__CLASS__,$sConfig);
+class sgbd_pdo_mysql extends abstract_sgbd_pdo
+{
+
+	public static function getInstance($sConfig)
+	{
+		return self::_getInstance(__CLASS__, $sConfig);
 	}
-	
-	public function getListColumn($sTable){
-		$pRs=$this->query(sgbd_syntax_mysql::getListColumn($sTable));
-		$tObj=array();
-		
-		if(empty($pRs)){
+
+	public function getListColumn($sTable)
+	{
+		$pRs = $this->query(sgbd_syntax_mysql::getListColumn($sTable));
+		$tObj = array();
+
+		if (empty($pRs)) {
 			return null;
 		}
-		
-		while($tRow=$pRs->fetch(PDO::FETCH_NUM)){
-			$tObj[]=$tRow[0];
+
+		while ($tRow = $pRs->fetch(PDO::FETCH_NUM)) {
+			$tObj[] = $tRow[0];
 		}
 		return $tObj;
 	}
-	public function getListTable(){
-		$pRs=$this->query(sgbd_syntax_mysql::getListTable());
-		
-		if(empty($pRs)){
+	public function getListTable()
+	{
+		$pRs = $this->query(sgbd_syntax_mysql::getListTable());
+
+		if (empty($pRs)) {
 			return null;
 		}
-		
-		$tObj=array();
-		while($tRow=$pRs->fetch(PDO::FETCH_NUM)){
-			$tObj[]=$tRow[0];
+
+		$tObj = array();
+		while ($tRow = $pRs->fetch(PDO::FETCH_NUM)) {
+			$tObj[] = $tRow[0];
 		}
 		return $tObj;
 	}
-	public function getInsertFromTab($tProperty){
-		
-		$sCols='';
-		$sVals='';
-		
-		if($tProperty){
-			$tKey=array_keys($tProperty);
-			foreach($tKey as $sVar){
-				$sCols.='`'.$sVar.'`,';
-				$sVals.='?,';
+	public function getInsertFromTab($tProperty)
+	{
+
+		$sCols = '';
+		$sVals = '';
+
+		if ($tProperty) {
+			$tKey = array_keys($tProperty);
+			foreach ($tKey as $sVar) {
+				$sCols .= '`' . $sVar . '`,';
+				$sVals .= '?,';
 			}
 		}
-		return '('.substr($sCols,0,-1).') VALUES ('.substr($sVals,0,-1).') ';
+		return '(' . substr($sCols, 0, -1) . ') VALUES (' . substr($sVals, 0, -1) . ') ';
 	}
-	public function getUpdateFromTab($tProperty){
-		$sReq='';
-		if($tProperty){
-			$tKey=array_keys($tProperty);
-			foreach($tKey as $sVar){
-				$sReq.='`'.$sVar.'`'.'=?,';
+	public function getUpdateFromTab($tProperty)
+	{
+		$sReq = '';
+		if ($tProperty) {
+			$tKey = array_keys($tProperty);
+			foreach ($tKey as $sVar) {
+				$sReq .= '`' . $sVar . '`' . '=?,';
 			}
 		}
-		
-		return substr($sReq,0,-1);
+
+		return substr($sReq, 0, -1);
 	}
-	protected function connect(){
-		if(empty($this->_pDb)){
-			$this->_pDb=new PDO(
-						$this->_tConfig[$this->_sConfig.'.dsn'],
-						$this->_tConfig[$this->_sConfig.'.username'],
-						$this->_tConfig[$this->_sConfig.'.password']
-			);
+	protected function connect()
+	{
+		if (empty($this->_pDb)) {
+
+			if (isset($this->_tConfig[$this->_sConfig . '.ssl_ca'])) {
+
+				$options = [
+					PDO::MYSQL_ATTR_SSL_CA => $this->_tConfig[$this->_sConfig . '.ssl_ca'],
+					PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => true,
+					PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+				];
+
+				$this->_pDb = new PDO(
+
+					$this->_tConfig[$this->_sConfig . '.dsn'],
+					$this->_tConfig[$this->_sConfig . '.username'],
+					$this->_tConfig[$this->_sConfig . '.password'],
+					$options
+
+				);
+			} else {
+
+				$this->_pDb = new PDO(
+					$this->_tConfig[$this->_sConfig . '.dsn'],
+					$this->_tConfig[$this->_sConfig . '.username'],
+					$this->_tConfig[$this->_sConfig . '.password']
+				);
+			}
 		}
 	}
-	
-	public function getLastInsertId(){
+
+	public function getLastInsertId()
+	{
 		return $this->_pDb->lastInsertId();
 	}
-	
-	public function getWhereAll(){
+
+	public function getWhereAll()
+	{
 		return '1=1';
 	}
-	
 }
